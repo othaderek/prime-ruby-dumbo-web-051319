1fe8a32ccb5e23725272c1eb1a9b03ecfc6fdcea
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-22 22:06:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,8 @@
 def prime?(num)
   if num == 0 
     return false
+    elsif num == 4
+    return false
     elsif 
     num > 1 && num % 2 == 0 || num % 3 == 0 || num % 11 == 0 || num % 105557 == 0
     return true
